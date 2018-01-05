ddf11d66398c39b87ad3aacfa768f4a38adbadec
Igor Savin <isavin@lighthousenetwork.com>
2018-01-05 19:34:10 +0200

    WIP refs

--- test/objection-swagger.spec.js
+++ test/objection-swagger.spec.js
@@ -1,5 +1,6 @@
 const assert = require('chai').assert;
 const sinon = require('sinon');
+const mkdirp = require('mkdirp');
 
 const Options = require('../lib/Options');
 const fileWriter = require('../lib/file-writer');
@@ -31,72 +32,81 @@ describe('objection-swagger', () => {
 		global.sandbox.restore();
 	});
 
-	it('generates model schema yaml from single model', async () => {
-		const result = objectionSwagger.generateSchema(SimpleModel);
+	describe('generateSchema', () => {
+		it('generates model schema yaml from single model', async () => {
+			const result = objectionSwagger.generateSchema(SimpleModel);
 
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'SimpleModel');
-		assert.equal(result[0].schema, SIMPLE_MODEL_SCHEMA);
-	});
-
-	it('generates model schema yaml from single model, excludes internal fields', async () => {
-		const result = objectionSwagger.generateSchema(SimpleModel, { excludeInternalData: true });
-
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'SimpleModel');
-		assert.equal(result[0].schema, SIMPLE_MODEL_SCHEMA_NO_INTERNAL);
-	});
-
-	it('generates model schema yaml from array of models', async () => {
-		const result = objectionSwagger.generateSchema([SimpleModel]);
-
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'SimpleModel');
-		assert.equal(result[0].schema, SIMPLE_MODEL_SCHEMA);
-	});
-
-	it('saves model schema yaml', async () => {
-		let writeResult;
-		const writeStub = sinon.stub(fileWriter, 'writeAll').callsFake((writeParams) => {
-			writeResult = writeParams;
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'SimpleModel');
+			assert.equal(result[0].schema, SIMPLE_MODEL_SCHEMA);
 		});
 
-		const result = objectionSwagger.saveSchema(SimpleModel, 'dummyDir');
+		it('generates model schema yaml from single model, excludes internal fields', async () => {
+			const result = objectionSwagger.generateSchema(SimpleModel, { excludeInternalData: true });
 
-		assert.lengthOf(writeResult, 1);
-		assert.equal(writeResult[0].targetFile, 'dummyDir/SimpleModel.yaml');
-		assert.equal(writeResult[0].data, SIMPLE_MODEL_SCHEMA);
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'SimpleModel');
+			assert.equal(result[0].schema, SIMPLE_MODEL_SCHEMA_NO_INTERNAL);
+		});
+
+		it('generates model schema yaml from array of models', async () => {
+			const result = objectionSwagger.generateSchema([SimpleModel]);
+
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'SimpleModel');
+			assert.equal(result[0].schema, SIMPLE_MODEL_SCHEMA);
+		});
+
+		it('saves model schema yaml', async () => {
+			let writeResult;
+			const writeStub = sinon.stub(fileWriter, 'writeAll').callsFake((writeParams) => {
+				writeResult = writeParams;
+			});
+
+			const result = objectionSwagger.saveSchema(SimpleModel, 'dummyDir');
+
+			assert.lengthOf(writeResult, 1);
+			assert.equal(writeResult[0].targetFile, 'dummyDir/SimpleModel.yaml');
+			assert.equal(writeResult[0].data, SIMPLE_MODEL_SCHEMA);
+		});
+
+		it('ignores private fields', async () => {
+			const result = objectionSwagger.generateSchema(ModelWithPrivateFields);
+
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'ModelWithPrivateFields');
+			assert.equal(result[0].schema, MODEL_WITH_PRIVATE_FIELDS_SCHEMA);
+		});
+
+		it('generates parent model schema correctly', async () => {
+			const result = objectionSwagger.generateSchema(ParentModel);
+
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'ParentModel');
+			assert.equal(result[0].schema, PARENT_MODEL);
+		});
+
+		it('generates parent model schema without internal fields correctly', async () => {
+			const result = objectionSwagger.generateSchema(ParentModel, { excludeInternalData: true });
+
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'ParentModel');
+			assert.equal(result[0].schema, PARENT_MODEL_NO_INTERNAL);
+		});
+
+		it('generates child model schema correctly', async () => {
+			const result = objectionSwagger.generateSchema(ChildModel);
+
+			assert.lengthOf(result, 1);
+			assert.equal(result[0].name, 'ChildModel');
+			assert.equal(result[0].schema, CHILD_MODEL);
+		});
 	});
 
-	it('ignores private fields', async () => {
-		const result = objectionSwagger.generateSchema(ModelWithPrivateFields);
-
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'ModelWithPrivateFields');
-		assert.equal(result[0].schema, MODEL_WITH_PRIVATE_FIELDS_SCHEMA);
-	});
-
-	it('generates parent model schema correctly', async () => {
-		const result = objectionSwagger.generateSchema(ParentModel);
-
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'ParentModel');
-		assert.equal(result[0].schema, PARENT_MODEL);
-	});
-
-	it('generates parent model schema without internal fields correctly', async () => {
-		const result = objectionSwagger.generateSchema(ParentModel, { excludeInternalData: true });
-
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'ParentModel');
-		assert.equal(result[0].schema, PARENT_MODEL_NO_INTERNAL);
-	});
-
-	it('generates child model schema correctly', async () => {
-		const result = objectionSwagger.generateSchema(ChildModel);
-
-		assert.lengthOf(result, 1);
-		assert.equal(result[0].name, 'ChildModel');
-		assert.equal(result[0].schema, CHILD_MODEL);
+	describe('saveSchema', () => {
+		it('saves model schema yaml from single model', async () => {
+			await mkdirp('build');
+			await objectionSwagger.saveSchema(ParentModel, 'build', {useEntityRefs: true});
+		});
 	});
 });
