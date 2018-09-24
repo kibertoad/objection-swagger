506e828540a3faccb07bf27b51e06aa05ea8b9b3
Igor Savin <iselwin@gmail.com>
2018-09-24 16:22:56 +0200

    Remove dependency on objection.js

---
+++ test/constants.spec.js
@@ -0,0 +1,19 @@
+const assert = require("chai").assert;
+const Model = require("objection").Model;
+const constants = require("../lib/constants");
+
+describe("constants", () => {
+  it("are in sync with Objection.js", () => {
+    assert.equal(
+      constants.BelongsToOneRelation,
+      Model.BelongsToOneRelation.name
+    );
+    assert.equal(constants.HasManyRelation, Model.HasManyRelation.name);
+    assert.equal(constants.HasOneRelation, Model.HasOneRelation.name);
+    assert.equal(
+      constants.HasOneThroughRelation,
+      Model.HasOneThroughRelation.name
+    );
+    assert.equal(constants.ManyToManyRelation, Model.ManyToManyRelation.name);
+  });
+});
